672ce939bad9c60eaf4ba13b9f31a9a0deef9771
Park Sueun <21900280@handong.edu>
2020-05-08 22:39:51 +0900

    Create search.c

---
+++ search.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "corona.h"
+
+// 검색 메뉴 함수, 어떤 방식으로 검색할 것인지 사용자에게 물어 return
+int selectSearchMenu(){
+
+}
+
+// 이름으로 검색
+void searchByName(Corona* c[], int count){
+
+}
+
+// 성별로 검색
+void searchByGender(Corona* c[], int count){
+
+}
+
+// 나이로 검색
+void searchByAge(Corona* c[], int count){
+
+}
+
+// 지역으로 검색
+void searchByResidence(Corona* c[], int count){
+
+}
+
+// 확진일로 검색
+void searchByDate(Corona* c[], int count){
+
+}
+
+// 격리시설로 검색
+void searchByHospital(Corona* c[], int count){
+
+}
+
+// 국내감염여부로 검색
+void searchByDomestic(Corona* c[], int count){
+
+}
